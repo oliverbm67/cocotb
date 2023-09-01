1d3f9b718aaccc26d1f0c3fd452c35369c4139b9
Olivier Betschi <olivier.betschi@protonmail.com>
2023-09-01 20:21:44 +0200

    Add a sanity test so that simulation will be running

--- examples/mixed_language_multi_libraries/cocotb/mixed_language_multi_libraries.py
+++ examples/mixed_language_multi_libraries/cocotb/mixed_language_multi_libraries.py
@@ -1,7 +1,24 @@
 """A compilation example for hdl mixing Verilog and VHDL and multiple libraries"""
 
 import os
+import cocotb
 from cocotb.runner import get_runner
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.clock import Clock
+
+@cocotb.test()
+async def sanity_test(dut):
+    """A simple test of the RTL to confirm the simulation is running"""
+    cocotb.start_soon(Clock(dut.clk, 1, units="ns").start())
+    dut.rstn.value = 0
+    dut.sig_in.value = 0
+    await Timer(1, units="ns")
+    await RisingEdge(dut.clk)
+    assert dut.sig_out.value == 0
+    dut.sig_in.value = 1
+    await RisingEdge(dut.clk)
+    assert dut.sig_out.value == 15
+
 
 
 def mixed_lang_multi_lib_runner():
