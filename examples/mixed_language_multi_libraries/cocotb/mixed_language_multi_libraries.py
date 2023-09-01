ffe7c9443ed1daab56b4eeb910f8ea095f4c7a8f
Olivier Betschi <olivier.betschi@protonmail.com>
2023-09-01 20:08:01 +0200

    Fixed typo and use black for formatting

--- examples/mixed_language_multi_libraries/cocotb/mixed_language_multi_libraries.py
+++ examples/mixed_language_multi_libraries/cocotb/mixed_language_multi_libraries.py
@@ -3,28 +3,27 @@
 import os
 from cocotb.runner import get_runner
 
+
 def mixed_lang_multi_lib_runner():
+    """cocotb python runner"""
     hdl_toplevel_lang = "vhdl"
     sim = "questa"
 
-    rtl_dir = os.getcwd()+"/../hdl/"
+    rtl_dir = os.getcwd() + "/../hdl/"
 
     runner = get_runner(sim)
 
     runner.build(
-            verilog_sources = [ rtl_dir+"reg_b.v" ],
-            vhdl_sources = [ rtl_dir+"reg_b.vhd" ],
-            hdl_library = "cell_lib"
+        verilog_sources=[rtl_dir + "reg_b.v"],
+        vhdl_sources=[rtl_dir + "reg_b.vhd"],
+        hdl_library="cell_lib",
     )
     runner.build(
-            verilog_sources = [ rtl_dir+"reg_a.v" ],
-            vhdl_sources = [ rtl_dir+"reg_a.vhd" ]
+        verilog_sources=[rtl_dir + "reg_a.v"], vhdl_sources=[rtl_dir + "reg_a.vhd"]
     )
-    runner.build(
-            vhdl_sources = [ rtl_dir+"top.vhd" ],
-            hdl_toplevel = "top"
-    )
-    runner.test(hdl_toplevel="top", test_module="mixed-langages-multi-lib")
+    runner.build(vhdl_sources=[rtl_dir + "top.vhd"], hdl_toplevel="top")
+    runner.test(hdl_toplevel="top", test_module="mixed_language_multi_libraries")
+
 
 if __name__ == "__main__":
     mixed_lang_multi_lib_runner()
